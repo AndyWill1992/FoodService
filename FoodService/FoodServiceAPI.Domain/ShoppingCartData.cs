427f41946398dae17b0973544e5d48dee883fa76
Andy Williamson <awilliamson15@qub.ac.uk>
2019-05-12 14:48:37 +0100

    Addition of Cart and User Objects

---
+++ FoodService/FoodServiceAPI.Domain/ShoppingCartData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodServiceAPI.Domain
+{
+    public class ShoppingCartData
+    {
+        public int TempOrderID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
